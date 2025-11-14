400c334f73c86d8157e4a2766ef945139dd57732
moshito <mosho@me.com>
2025-11-14 21:20:27 +0100

    Added Matrix rain

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ import { Timeline } from "./components/Timeline";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
+import MatrixRain from "react-matrix-rain"
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white overflow-x-hidden">
       {/* Background pattern */}
       <div className="fixed inset-0 bg-[radial-gradient(ellipse_at_top,rgba(0,255,65,0.05),transparent_50%)] pointer-events-none" />
-      
       {/* Main content */}
       <div className="relative z-10">
+      <MatrixRain color="#00ff0020" />
         <Hero />
         <SkillsTags />
         <About />
